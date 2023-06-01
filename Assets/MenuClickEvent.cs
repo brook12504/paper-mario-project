ad8ea8bf594039e88c6ea65c15026921009f559b
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2023-06-01 20:42:35 +0900

    UI 패키지 임포트
    메인 타이틀씬, 스코어 버튼, 설정 버튼, 종료 버튼 이벤트 추가

---
+++ Assets/MenuClickEvent.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuClickEvent : MonoBehaviour
+{
+    public void SettingClickEvent() {
+        SceneManager.LoadScene("Setting");
+    }
+
+    public void ScoreClickEvent() {
+        SceneManager.LoadScene("ScoreView");
+    }
+
+    public void BackToLobby() {
+        SceneManager.LoadScene("GameTitle");
+    }
+
+    public void StartClickEvent() {
+        
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
