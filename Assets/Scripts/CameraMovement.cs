ec28fe163144ba4ee5d9d7bb6741aa2ed6d58e28
최길한 <69311223+brook12504@users.noreply.github.com>
2023-06-05 08:11:27 +0900

    카메라 캐릭터 이동에 따라 같이 이동

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject go_Target;
+
+    [SerializeField]
+    private float speed;
+
+    private Vector3 difValue;
+
+    void Start()
+    {
+        difValue = transform.position - go_Target.transform.position;
+    
+    }
+
+     // Update is called once per frame
+    void LateUpdate()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, go_Target.transform.position + difValue, Time.deltaTime * speed); 
+    }
+}
