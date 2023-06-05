b0bfbaff03a7f2679a331f59cdb3db7c22278af5
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2023-06-06 01:35:24 +0900

    일시정지 메뉴와 그 외 메뉴 클릭이벤트 구현
    일시정지 메뉴와 그 외 메뉴 클릭이벤트 구현

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 다이얼로그식 일시정지 메뉴 관련 클래스 
+
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject menuDialog; // 메뉴 다이얼로그를 담을 게임 오브젝트 변수
+    public GameObject soundDialog;
+
+    private bool isMenuOpen = false; // 메뉴 다이얼로그가 열려있는지 여부를 저장하는 변수
+    private bool isSoundOpen = false;
+
+    private void Update()
+    {
+        // ESC 키 입력 감지
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // 메뉴 다이얼로그의 활성화 상태 변경
+            if (isMenuOpen)
+            {
+                CloseMenu();
+            }
+            else
+            {
+                OpenMenu();
+            }
+        }
+    }
+
+    private void OpenMenu()
+    {
+        // 메뉴 다이얼로그 활성화
+        menuDialog.SetActive(true);
+
+        // 게임 일시정지
+        Time.timeScale = 0f;
+
+        // 메뉴 상태 업데이트
+        isMenuOpen = true;
+    }
+
+    public void CloseMenu()
+    {
+        // 메뉴 다이얼로그 비활성화
+        menuDialog.SetActive(false);
+        soundDialog.SetActive(false);
+
+        // 게임 재개
+        Time.timeScale = 1f;
+
+        // 메뉴 상태 업데이트
+        isMenuOpen = false;
+        isSoundOpen = false;
+    }
+
+    public void SoundDialogOpen() 
+    {
+        menuDialog.SetActive(false);
+
+        soundDialog.SetActive(true);
+        isSoundOpen = true;
+    }
+
+    public void SoundDialogClose()
+    {
+        menuDialog.SetActive(true);
+        isMenuOpen = true;
+
+        soundDialog.SetActive(false);
+        isSoundOpen = false;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     public GameObject camera2D; // 2D 카메라
     public GameObject camera3D; // 3D 카메라
 
@@ -12,7 +11,7 @@ public class PlayerController : MonoBehaviour
 
     private bool is2D = true; // 현재 시점이 2D인지 여부
     bool isGrounded = true;
-    bool isPaused = false;
+
     private Rigidbody rb3D;
 
     void Start()
@@ -28,26 +27,14 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             is2D = !is2D;
-            StartCoroutine(WaitAndExecute()); //1초 대기후 명령어 실행
-            
+            camera2D.SetActive(is2D);
+            camera3D.SetActive(!is2D);
         }
         //점프 기능
         if(Input.GetButtonDown("Jump") && isGrounded){
             rb3D.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
             isGrounded = false;
         }
-        //esc가 눌렸을 때 시간이 멈추게 하기.
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            TogglePause();
-        }
-    }
-    //1초 대기후 카메라 시점 변환 명령어를 실행하기 위해 삽입
-    IEnumerator WaitAndExecute()
-    {
-        yield return new WaitForSeconds(1f);
-        camera2D.SetActive(is2D);
-        camera3D.SetActive(!is2D);
     }
 
     private void FixedUpdate()
@@ -68,26 +55,10 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         // 바닥과 충돌 체크
         if (collision.gameObject.CompareTag("Ground"))
             isGrounded = true;
-    }
-
-    private void TogglePause()
-    {
-        isPaused = !isPaused;
-
-        if (isPaused)
-        {
-            // 시간을 멈추는 코드
-            Time.timeScale = 0f;
-        }
-        else
-        {
-            // 시간을 다시 움직이도록 설정하는 코드
-            Time.timeScale = 1f;
-        }
     }
 }
