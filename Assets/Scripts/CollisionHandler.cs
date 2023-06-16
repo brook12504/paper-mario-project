12b906fcf4f5568732089a31cf4a435dfe649902
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2023-06-16 12:18:59 +0900

    별 먹는 소리 추가
    사운드업데이트, Colilision핸들러 스크립트에서 5번줄부터 14번줄 까지 (원래있었던 스크립트) 지워서 머지해주세용

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,14 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+
 
 public class CollisionHandler : MonoBehaviour
 {
-     private void OnCollisionEnter(Collision collision)
+
+    private int gatheredStar = 0;
+    public AudioSource audioSource;
+
+    public float jumpForce = 5f;
+    public float jumpDuration = 0.5f;
+    public float destroyDelay = 0.5f;
+
+    private Rigidbody rb;
+
+
+    
+
+    private void Start()
     {
-        if (collision.gameObject.CompareTag("Player"))
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
         {
-            gameObject.SetActive(false);
+            PlayEarnSound();
+            StartCoroutine(JumpAndDestroy());
+        }
+    }
+
+    private IEnumerator JumpAndDestroy()
+    {
+        // 위로 튀어오르는 동작
+        rb.useGravity = true;
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+        yield return new WaitForSeconds(jumpDuration);
+
+        // 일정 시간이 지난 후 오브젝트를 비활성화
+        gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(destroyDelay);
+
+        CollectStar();
+    }
+
+    private void CollectStar()
+    {
+        // 코인을 제거
+        gatheredStar++;
+        Debug.Log("별 먹음");
+        gameObject.SetActive(false);
+    }
+
+    public int GetStar() {
+        return gatheredStar;
+    }
+
+    public void ResetStar() {
+        gatheredStar = 0;
+    }
+
+    private void PlayEarnSound()
+    {
+        if (audioSource != null)
+        {
+            Debug.Log("별먹기 사운드");
+            audioSource.Play();
         }
     }
 }
